e663d9cb3de22933716bebf14f422efd865ec780
adir shaban <adirshaban@github.com>
2017-12-06 15:23:34 +0200

    Adding downloading directory checks

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
-
 const download = require('download');
 const fs = require('fs');
 const fetch = require('node-fetch');
+const path = require('path');
+
+const saveToDir = "Downloads";
 const allExtentionsUrl = "https://marketplace.visualstudio.com/_apis/public/gallery/extensionquery"
 const pageSize = 300;
 const initialPage = 1;
 
-const fetchExtentionsPage = (pageNumber) => {
+const validateDownloadLocation = (downloadsLocation, cb) => {
+    const dir = path.resolve(__dirname, downloadsLocation);
+    
+    if (!fs.existsSync(dir)) {
+        console.log("Creating the directory", dir);
+        fs.mkdirSync(dir, (dir)=> {
+            console.log("Created directory :", dir);
+        });
+    } else {
+        console.log("Saving extensions into :", dir);
+    }
+
+    cb();
+};
+
+const fetchExtentionsPage = (pageNumber, downloadsDir) => {
     console.log("fetching page number", pageNumber)
+
+    // This is the data sent in the body request to the marketplace
     const data = {assetTypes:[null],filters:[{criteria:[{filterType:8,value:"Microsoft.VisualStudio.Code"},{"filterType":12,"value":"5122"}],direction:2,sortBy:4,pageSize,pageNumber}], flags: 870};
+    
     fetch(allExtentionsUrl, {
         method: "POST",
         body: JSON.stringify(data),
@@ -20,42 +40,46 @@ const fetchExtentionsPage = (pageNumber) => {
     }).then(function(response) {
         return response.json();
     }).then(data => {
-        console.log(data);
-        console.log("downloading", data.results[0].extensions.length, " extentions")
+        console.log("downloading", data.results[0].extensions.length, "extentions")
         for (let extension of data.results[0].extensions) {
-
             const publisherName = extension.publisher.publisherName;
             const extName = extension.extensionName;
             const lastVersion = extension.versions[0].version;
+
+            // This is a template of each vsix file download link contains the params
             const downloadUrl = `https://${publisherName}.gallery.vsassets.io/_apis/public/gallery/publisher/${publisherName}/extension/${extName}/${lastVersion}/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage`;
+            
+            // This will be the vsix filename after the download
             const fileName = `${publisherName}.${extName}-${lastVersion}.vsix`;
+        
             try {
-                downloadExt(downloadUrl, fileName, () => {
+                downloadExt(downloadUrl, fileName, downloadsDir, () => {
+                    console.log("downloaded", extName,"(", lastVersion, ") by", publisherName);
                 });
             } catch(err) {
                 console.warn(fileName, err);
             }
         }
 
+        // Checks if the page we asked is full to known if needed another run
         if (data.results[0].extensions.length == pageSize) {
             setTimeout(() => fetchExtentionsPage(pageNumber + 1), 5000);
         }
     })
 };
 
-const downloadExt = (url, filename, cb) => {
-    
+const downloadExt = (url, fileName, downloadsDir, cb) => {
     download(url).then(data => {
-        fs.writeFile(`mostPopularExtentions/${filename}`, data, (err) => {
-             if(err) 
-                console.log("err in", filename, " :", err);
-            else
-                console.log(filename, "downloaded");
+        fs.writeFile(`${downloadsDir}/${fileName}`, data, (err) => {
+            if(err) 
+                console.log("err in", fileName, " :", err);
+            else {
+                console.log(fileName);
+                cb();
+            }
         });
-
-        cb();
 
     }).catch(err => console.log(err));
 }
 
-fetchExtentionsPage(initialPage);
+validateDownloadLocation(saveToDir, () => {fetchExtentionsPage(initialPage, saveToDir)})
